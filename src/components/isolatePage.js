1cd0606aacbb53230a3daae535449158ed0ef660
Daniel Anderson <43540934+Danderson123@users.noreply.github.com>
2021-03-23 14:53:36 +0000

    Source elasticsearch API information from secrets

--- src/components/isolatePage.js
+++ src/components/isolatePage.js
@@ -10,8 +10,7 @@ const IsolatePage = ({ match }) => {
     const [searched, setSearched] = useState(false)
     const [searchResult, updateResult] = useState();
 
-    const index = process.env.REACT_APP_ISOLATE_INDEX
-    const searchURL = process.env.REACT_APP_API_URL + index + "/_search"
+    const searchURL = process.env.REACT_APP_API_URL + "sparc_isolate_index/_search"
     const apiKey = process.env.REACT_APP_API_KEY
 
     const obj =  {
