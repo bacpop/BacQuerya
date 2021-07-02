bf16507a2125405d754110b5252cac3198c7ece9
Jeff Sweeney <jsweeney.dev@digitalsafe.com>
2021-07-02 06:23:36 -0400

    Made a common header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import {useState, useEffect} from "react"
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 
-import AboutPage from './components/aboutPage'
+import Header from './components/header'
 import SearchPage from './components/searchPage'
 import StudyPage from './components/loadURLs/studyPage'
 import GenePage from './components/loadURLs/genePage'
@@ -12,34 +11,23 @@ import GenusPage from './components/loadURLs/genusPage'
 import IsolatePage from './components/loadURLs/isolatePage'
 import submissionPage from './components/displayPages/submissionDisplay'
 
-function App() {
-
-  //resize component on window resize
-  const [windowWidth, setWidth] = useState(window.innerWidth)
-  const [windowHeight, setHeight] = useState(window.innerHeight)
-  const updateDimensions = () => {
-      setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
-  };
-  useEffect(() => {
-      window.addEventListener("resize", updateDimensions);
-      return () => window.removeEventListener("resize", updateDimensions);
-  }, []);
-
+function App () {
   return (
-    <main className="App" style={{height: windowHeight + "px", width: windowWidth + " px"}}>
-      <Router>
-        <Route exact path="/" component={SearchPage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/study/:encodedDOI" component={StudyPage} />
-        <Route exact path="/gene/:geneName" component={GenePage} />
-        <Route exact path="/isolate/:Genus/:Species/:BioSample" component={IsolatePage} />
-        <Route exact path="/isolate/:Genus/:Species" component={SpeciesPage} />
-        <Route exact path="/isolate/:Genus/" component={GenusPage} />
-        <Route exact path="/submit/:encodedDOI/" component={submissionPage} />
-      </Router>
-    </main>
-  );
+    <>
+      <Header />
+      <main className='App'>
+        <Router>
+          <Route exact path='/' component={SearchPage} />
+          <Route exact path='/study/:encodedDOI' component={StudyPage} />
+          <Route exact path='/gene/:geneName' component={GenePage} />
+          <Route exact path='/isolate/:Genus/:Species/:BioSample' component={IsolatePage} />
+          <Route exact path='/isolate/:Genus/:Species' component={SpeciesPage} />
+          <Route exact path='/isolate/:Genus/' component={GenusPage} />
+          <Route exact path='/submit/:encodedDOI/' component={submissionPage} />
+        </Router>
+      </main>
+    </>
+  )
 };
 
-export default App;
+export default App
