68ac028a5eb6d89fae7c9f6c493726ed6b7946b8
Jeff Sweeney <jsweeney.dev@digitalsafe.com>
2021-07-05 06:52:00 -0400

    Page title updates when navigating

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 import Header from './components/header'
-import SearchPage from './components/searchPage'
-import StudyPage from './components/loadURLs/studyPage'
-import GenePage from './components/loadURLs/genePage'
-import SpeciesPage from './components/loadURLs/speciesPage'
-import GenusPage from './components/loadURLs/genusPage'
-import IsolatePage from './components/loadURLs/isolatePage'
-import submissionPage from './components/displayPages/submissionDisplay'
+import SearchPageBase from './components/searchPage'
+import StudyPageBase from './components/loadURLs/studyPage'
+import GenePageBase from './components/loadURLs/genePage'
+import IsolatePageBase from './components/loadURLs/isolatePage'
+import SubmissionPageBase from './components/displayPages/submissionDisplay'
+
+const createTitleWrapper = (Component, title = 'Bacquerya') => (props) => {
+  useEffect(() => {
+    document.title = title
+  }, [])
+  return <Component {...props} />
+}
+
+const SearchPage = createTitleWrapper(SearchPageBase, 'Bacquerya - Search')
+const StudyPage = createTitleWrapper(StudyPageBase, 'Bacquerya - Study')
+const GenePage = createTitleWrapper(GenePageBase, 'Bacquerya - Gene')
+const IsolatePage = createTitleWrapper(IsolatePageBase, 'Bacquerya - Isolate')
+const SubmissionPage = createTitleWrapper(SubmissionPageBase, 'Bacquerya - Submit')
 
 function App () {
   return (
@@ -21,9 +33,7 @@ function App () {
           <Route exact path='/study/:encodedDOI' component={StudyPage} />
           <Route exact path='/gene/:geneName' component={GenePage} />
           <Route exact path='/isolate/:Genus/:Species/:BioSample' component={IsolatePage} />
-          <Route exact path='/isolate/:Genus/:Species' component={SpeciesPage} />
-          <Route exact path='/isolate/:Genus/' component={GenusPage} />
-          <Route exact path='/submit/:encodedDOI/' component={submissionPage} />
+          <Route exact path='/submit/:encodedDOI/' component={SubmissionPage} />
         </Router>
       </main>
     </>
