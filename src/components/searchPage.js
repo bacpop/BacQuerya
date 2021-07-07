479f6e300d9af4b76092a80e989776d09692eafb
Danderson123 <danielanderson1@hotmail.com>
2021-07-07 12:45:25 -0400

    Correct read and assembly download link short URLs

--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -21,6 +21,27 @@ const cleanAliases = (names, consistentName) => {
   return cleanedNames
 }
 
+const shortLinks = (representation, URLs, index) => {
+  const seqs = URLs instanceof Array
+                ? URLs
+                : [URLs]
+  if (seqs.length === 1) {
+    if (representation === 'full') {
+      return 'Assembly'
+    }
+    return 'Read'
+  }
+  if (seqs.length === 2) {
+    return "Read_" + index + 1
+  }
+  if (seqs.length === 3) {
+    if (index !== 2) {
+      return "Read_" + (index + 1) + ", "
+    }
+    return "Assembly"
+  }
+}
+
 const splitGeneNames = (result) => {
   const panarooNames = result._source.panarooNames || ''
   const consistentName = result._source.consistentNames
@@ -93,19 +114,18 @@ const typeRequest = {
               r._source.sequenceURL instanceof Array
                 ? r._source.sequenceURL
                 : [r._source.sequenceURL]
-            ).map(link => (
-              <a
-                key={link}
-                href={link}
-                rel='noreferrer'
-                title={link.split('/')[link.split('/').length - 1]}
-              >
-                {
-                  r._source.Genome_representation === 'full'
-                    ? 'Assembly'
-                    : 'Reads'
-                }
-              </a>
+            ).map((link, index) => (
+                <a
+                  key={link}
+                  href={link}
+                  rel='noreferrer'
+                  title={link.split('/')[link.split('/').length - 1]}
+                >
+                  {shortLinks(r._source.Genome_representation,
+                              r._source.sequenceURL,
+                              index)
+                  }
+                </a>
             ))
           }
         </>
